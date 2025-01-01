41dff61f6b588ec066ee947cee9f7c8ac97b9147
Eric Auer <ericauer86@gmail.com>
2025-01-01 18:15:05 +0100

    Issue 2 : Mise à jour automatique des points du club + test_update_points.py

---
+++ tests/test_show_clubs.py
@@ -0,0 +1,12 @@
+import pytest
+from server import app
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_show_clubs(client):
+    response = client.get('/showClubs')
+    assert response.status_code == 200
+    assert b'Clubs' in response.data  # Vérifier que le mot 'Clubs' est dans la réponse
