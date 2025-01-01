29a0b2a3147ad9787f976c53683d0da24c4b1e1d
Eric Auer <ericauer86@gmail.com>
2025-01-01 15:54:25 +0100

    Issue 1 : voir la liste des clubs ainsi que leurs points

--- server.py
+++ server.py
@@ -57,3 +57,7 @@ def purchasePlaces():
 @app.route('/logout')
 def logout():
     return redirect(url_for('index'))
+
+@app.route('/showClubs')
+def showClubs():
+    return render_template('clubs.html', clubs=clubs)
