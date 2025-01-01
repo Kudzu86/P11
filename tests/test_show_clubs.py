d77430519695722fee11bebf283b0f2cee89d9da
Eric Auer <ericauer86@gmail.com>
2025-01-01 18:33:25 +0100

    Issue 1 : ajout link club points sur la page d'acceuil + test_show_clubs.py

--- tests/test_show_clubs.py
+++ tests/test_show_clubs.py
@@ -10,3 +10,4 @@ def test_show_clubs(client):
     response = client.get('/showClubs')
     assert response.status_code == 200
     assert b'Clubs' in response.data  # Vérifier que le mot 'Clubs' est dans la réponse
+
